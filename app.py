0d9bf7fd2b95e3b3f49ea27d63a1ded610fba423
Yidi Huang <huangy897@gmail.com>
2023-08-23 12:19:02 -0400

    Update to use llama_index for document retrieval

--- app.py
+++ app.py
@@ -1,40 +1,46 @@
 import os
 import pandas as pd
 import random
+import torch
+
+from sentence_transformers import SentenceTransformer, util, models
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+from llama_index.llms import LlamaCPP
 
 idk = ["I am not sure...", "I am so sorry, I don't know.", "I am afraid I do not know how to answer this question.", "I don't know. Even though I am an old man, my knowledge is limited. Can you ask me something else?", "I am sorry, I do not know the answer to that. I died over 500 years ago, so there are many things that I don't know. Can you ask me something else?"]
 
-# Read docs
-f = open("questions.txt", "r")
-docs = f.readlines()
-f.close()
-
-g = open("themes.txt", "r")
-answerdoc = g.readlines()
-g.close()
-
 # Load LLaMa
 from llama_cpp import Llama
-llm = Llama(model_path="../llama.cpp/models/alpaca.13b.ggmlv3.q8_0.bin", n_ctx=2048)
-
+# llm = Llama(model_path="../llama.cpp/models/alpaca.13b.ggmlv3.q8_0.bin", n_ctx=2048)
 
 ## CLASS ##
-from sentence_transformers import SentenceTransformer, util, models
-import torch
 
 class Character:
 
-  def __init__(self, name, image_driver=None):
-
+  def __init__(self, name, docs_fp='questions.txt', answerdoc_fp='themes.txt'):
     self.s_prompt = ""
     self.name = name
-    self.image_driver = image_driver
-    self.embedder = SentenceTransformer('all-MiniLM-L6-v2')
-    self.corpus_embeddings = self.embedder.encode(docs, convert_to_tensor=True)
 
+    # Load docs
+    self.docs_fp = docs_fp
+    with open(docs_fp) as f:
+      self.docs = f.readlines()
+
+    with open(answerdoc_fp) as g:
+      self.answerdoc = g.readlines()
+
+    # LlamaIndex stuff
+    self.llm = LlamaCPP(
+        model_path="../llama.cpp/models/alpaca.13b.ggmlv3.q8_0.bin",
+        context_window=2048,
+        )
+    self.service_context = ServiceContext.from_defaults(llm=self.llm)
+    self.build_vector_index()
 
   def search_docs(self, query):
     # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+    self.embedder = SentenceTransformer('all-MiniLM-L6-v2')
+    self.corpus_embeddings = self.embedder.encode(self.docs, convert_to_tensor=True)
 
     top_k = min(5, len(docs))
     # print("These are the closest 2 sentences to the given query: \n\n", top_k)
@@ -58,6 +64,26 @@ class Character:
 
     return final_context[0], themes
 
+
+  def build_vector_index(self):
+    documents = SimpleDirectoryReader(input_files=[self.docs_fp]).load_data()
+    self.index = VectorStoreIndex.from_documents(documents, service_context=self.service_context)
+
+
+  def query_vector_index(self, query):
+    retriever = self.index.as_retriever(similarity_top_k=5)
+    top_results = retriever.retrieve(query)
+    combined_context = ''
+    i = 0
+
+    while len(combined_context) < 1800:
+      combined_context += top_results[i].node.text
+      i += 1
+    ## TODO retrieve real themes
+    dummy_themes = ['foo' for foo in top_results]
+
+    return combined_context, dummy_themes
+
   def style_transfer(self, context, themes, question, qa_pairs):
     self.s_prompt = ""
     for q, a in qa_pairs:
@@ -66,27 +92,18 @@ class Character:
     self.s_prompt += f'\n\nPatient question: {question}\nLLM prompt: '
     self.f_prompt = f"Below is an instruction that describes a task, paired with some inputs that provide further context. Write a response that appropriately completes the request.\n\n### Instruction:\nDetermine if any parts of the below context are relevant to the below question. If so, concisely summarize the relevant information from the context in bullet points. If the context is not relevant to the question, output 'not relevant.'\n\n### Context:\n{context}\n\n### Question:\n{question}\n\n### Response:\n"
 
-
-    try :
-        print(self.f_prompt)
-        self.completion = llm.create_completion(
-                                            prompt=self.f_prompt,
-                                            temperature=0.3,
-                                            max_tokens=512,
-                                            top_p=.2,
-                                            frequency_penalty=0,
-                                            presence_penalty=0,
-                                            stop=['Patient:']
-                                            )
-        # print(self.completion)
-        self.completion = self.completion["choices"][0]["text"]
-    except:
-        print("in except")
-        return random.choice(idk)
-    # self.completion = self.completion[:self.completion.rfind('.')]+'.'
+    print(self.f_prompt)
+    self.completion = self.llm.complete(
+       self.f_prompt,
+        temperature=0.3,
+        max_tokens=512,
+        top_p=.2,
+        frequency_penalty=0,
+        presence_penalty=0,
+        )
+    self.completion = self.completion.text
     self.s_prompt += self.completion +'\n'
     print("Completion: \n\n", self.completion)
-    # print(self.f_prompt)
     return self.completion
 
 c = Character(name='xyz')
@@ -97,5 +114,5 @@ if __name__ == '__main__':
         question = input("What question would you like to ask GenChat? \n Press q to quit\n\n")
         if question == 'q':
            break
-        context, themes = c.search_docs(question)
+        context, themes = c.query_vector_index(question)
         c.style_transfer(context, themes, question, [])
